e732bcec47320c7e3c75207fa14a26af17f376d6
Allen Mathews <allenjoemathews@gmail.com>
2021-11-05 15:53:39 +1100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import {BiArchive} from "react-icons/bi"
+import {BiCalendar} from "react-icons/bi"
 import "tailwindcss/tailwind.css"
+import Search from "./components/Search"
+import AddAppointment from "./components/AddAppointment"
+import appointmentList from "./data.json"
+import AppointmentInfo from "./components/AppointmentInfo"
 
 function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">
-      <h1 className="text-5xl">
-        <BiArchive className="inline-block text-red-400 align-top"/>Your Appointment</h1>
+      <h1 className="text-5xl mb-3">
+        <BiCalendar className="inline-block text-red-400 align-top"/>Your Appointment</h1>
+        <AddAppointment/>
+        <Search />
+
+        <ul className="divide-y divide-gray-200">
+           {appointmentList
+           .map(appointment => (
+            <AppointmentInfo key={appointment.id}
+            appointment={appointment}
+            />
+           )
+           
+           )}
+
+        </ul>
     </div>
   );
 }
